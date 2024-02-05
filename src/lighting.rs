d8a4a3939844885cc1893c9887421846f3b7e645
Jack Darlison <jdarlison7@gmail.com>
2024-02-05 13:31:58 +0000

    better ocean colour

--- src/lighting.rs
+++ src/lighting.rs
@@ -4,9 +4,9 @@ pub struct LightingPlugin;
 
 impl Plugin for LightingPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(ClearColor(Color::rgb(0.1, 0.3, 0.6)))
+        app.insert_resource(ClearColor(Color::rgb(0.0, 0.6, 1.0)))
             .insert_resource(AmbientLight {
-                brightness: 0.8,
+                brightness: 1.0,
                 ..default()
             });
     }
